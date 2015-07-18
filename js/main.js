d636f82f4dbc5f7049054ab3aa445137743abaac
Ben Tranter <bwtrante@lakeheadu.ca>
2015-07-17 22:20:13 -0400

    Add footer template, add browser check for window.history

--- js/main.js
+++ js/main.js
@@ -2,20 +2,28 @@
 
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
-    function callback(e) {
-      e = window.e || e;
-
-      if (e.target.tagName !== 'A') {
-        return;
-      }
-
-      console.log('a link was clicked');
-    }
+    console.log('History and pushState enabled: ' + pushStateExists());
 
     if (document.addEventListener) {
       document.addEventListener('click', callback, false);
     } else {
       document.attachEvent('onclick', callback);
     }
-  })
+
+    // Checks to see if pushState is supported by the 
+    // browser
+    function pushStateExists() {
+      return !!(window.history && window.history.pushState);
+    }
+
+    // Event bubbling for anchor elements
+    function callback(e) {
+      e = window.e || e;
+      if (e.target.tagName !== 'A') {
+        return;
+      }
+      // The function to execute when any link gets clicked
+      console.log('a link was clicked');
+    }
+  });
 })();
