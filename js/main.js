65a4c3c0ee55be5caa7cc6f95894be4dfd7b0eb3
Ben Tranter <bwtrante@lakeheadu.ca>
2015-07-18 01:46:43 -0400

    Intercept anchor clicks and replace with AJAX response

--- js/main.js
+++ js/main.js
@@ -1,29 +1,107 @@
 'use strict';
 
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    console.log('History and pushState enabled: ' + pushStateExists());
+/**
+ * Stacker is an AJAX + pushState + WebSocket lib for
+ * applications powered by Go.
+ *
+ * @author Ben Tranter
+ * @version 0.0.1
+ */
+var Stacker = {
 
+  /**
+   * Initializes this library by registering all clicks
+   * on the document and executing the callback.
+   */
+  init: function() {
     if (document.addEventListener) {
-      document.addEventListener('click', callback, false);
+      document.addEventListener('click', Stacker.callback, false);
     } else {
-      document.attachEvent('onclick', callback);
+      document.attachEvent('onclick', Stacker.callback);
     }
+  },
 
-    // Checks to see if pushState is supported by the 
-    // browser
-    function pushStateExists() {
-      return !!(window.history && window.history.pushState);
-    }
+  /**
+   * Callback for the click event that fires anywhere on
+   * the document. Used to register events on all anchor
+   * elements, so we can intercept that click to make an
+   * AJAX request.
+   *
+   * @param {Object} the click event
+   */
+  callback: function(e) {
+    e = window.e || e;
+    if (e.target.tagName !== 'A') return;
+    
+    e.preventDefault();
+    var tpl = Stacker.fetch(e.target.href);
+    console.log(tpl);
+  },
 
-    // Event bubbling for anchor elements
-    function callback(e) {
-      e = window.e || e;
-      if (e.target.tagName !== 'A') {
-        return;
-      }
-      // The function to execute when any link gets clicked
-      console.log('a link was clicked');
-    }
+  /**
+   * Checks for pushState support.
+   *
+   * @return {Boolean} whether the browser will support
+   * HTML5 pushState or not.
+   */
+  pushStateExists: function() {
+    return !!(window.history && window.history.pushState);
+  },
+
+  /**
+   * Checks for WebSocket support.
+   *
+   * @return {Boolean} whether the browser will support
+   * web sockets or not.
+   */
+  WebSocketExists: function() {
+    return typeof(WebSocket) === 'function';
+  },
+
+  /**
+   * The AJAX part of this library. Gets a compiled
+   * template from the server.
+   *
+   * @param {String} url of resource you need.
+   */
+  fetch: function(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+
+    xhr.setRequestHeader('Accept', 'text/html, application/xhtml+xml, application/xml');
+    xhr.setRequestHeader('X-XHR-Referer', document.location.href);
+    xhr.send();
+
+    // Events
+    xhr.onload = function() {
+      // @TODO: manage scroll position in here and stuff like that
+    };
+
+    xhr.onloadend = function() {
+      Stacker.replace(xhr.response);
+    };
+    xhr.onerror = function() {
+      // @TODO: Change this
+      window.alert('Error during fetch');
+    };
+  },
+
+  /**
+   * Replaces the contents of the DOM with the contents
+   * returned from the AJAX request.
+   *
+   * @param {String} the HTML from the response.
+   */
+  changePage: function(html) {
+    document.getElementsByTagName('body')[0].innerHTML = html;
+  }
+};
+
+(function() {
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    // Initialize our pushState lib
+    Stacker.init();
+    console.log(Stacker.pushStateExists(), Stacker.WebSocketExists());
   });
 })();
--- main.go
+++ main.go
@@ -20,10 +20,20 @@ type Page struct {
 }
 
 // homeHandler handles the home page. It also get's called
-// twice, because your browser asks for `favicon.ico`
+// twice, because your browser asks for `favicon.ico`.
+// This needs a serveMux to handle the browser asking for
+// `favicon.ico` with every request.
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.UserAgent() + "\n\n")
 	err := templates.ExecuteTemplate(w, "home.html", &Page{Title: "Test"})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// testHandler responds with the `test.html` template, to
+// be used with testing some front end code.
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "test.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -32,6 +42,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Println("Started")
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/test/", testHandler)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js/"))))
 	http.ListenAndServe(":3000", nil)
